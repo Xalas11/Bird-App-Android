826caa7e4fd349630fbac46e4cf8e0b4f1c81c95
xalas11 <mildenberger.robert@googlemail.com>
2015-06-27 11:20:54 +0200

    First commit

---
+++ platforms/android/assets/www/js/app/donwloadDemAll.js
@@ -0,0 +1,208 @@
+// sahne am ende noch ma gucken!
+
+var dataUrl;
+var fileNameDirectory;
+var a = 1;
+var b = 0;
+var c = a;
+var count = 0;
+var birdDownloadPath;
+var progress = 2;
+var progressInfo = 98.5 / birdNameChange.length;
+var downloading = false;
+var somethingWentWrong = false;
+
+function downReset() {
+	$("#downWrapID").show(); dataUrl;fileNameDirectory;
+	a = 1;
+	b = 0;
+	c = a;
+	count = 0; birdDownloadPath;
+	progress = 2;
+	progressInfo = 98.5 / birdNameChange.length;
+	downloading = false;
+	somethingWentWrong = false;
+	downDemAlll();
+}
+
+function downDemAlll() {
+	b = 0;
+	if (downloading == false) {
+		downloading = true;
+		if (lang == true) {
+			document.getElementById("downStop").innerHTML = "STOP";
+		} else {
+			document.getElementById("downStop").innerHTML = "VSTOP";
+		}
+
+	} else if (downloading == true) {
+		downloading = false;
+		if (lang == true) {
+			document.getElementById("downStop").innerHTML = "START";
+		} else {
+			document.getElementById("downStop").innerHTML = "VSTART";
+		}
+	}
+	if (allDonwloaded != "downloaded") {
+		downDemAlllSchleife();
+	}
+}
+
+function downLang() {
+
+	if (lang == true) {
+		if (downloading == true) {
+			document.getElementById("downStop").innerHTML = "VSTOP";
+		} else {
+			document.getElementById("downStop").innerHTML = "VSTART";
+		}
+		document.getElementById("downLang").innerHTML = "ENG";
+		document.getElementById("downHeader").innerHTML = "DOWNLOADING BIRD PICTURES PLEASE WAIT...";
+		document.getElementById("downHeader").innerHTML = "MAKE SURE YOUR WIFI IS ON, THE CONTENT IS AROUND 60MB.";
+		document.getElementById("downInfo").innerHTML = "KAMEHAMEHA";
+		lang = false;
+	} else if (lang == false) {
+		if (downloading == true) {
+			document.getElementById("downStop").innerHTML = "STOP";
+		} else {
+			document.getElementById("downStop").innerHTML = "START";
+		}
+		document.getElementById("downLang").innerHTML = "VIET";
+		document.getElementById("downHeader").innerHTML = "DOWNLOADING BIRD PICTURES PLEASE WAIT...";
+		document.getElementById("downInfo").innerHTML = "MAKE SURE YOUR WIFI IS ON, THE CONTENT IS AROUND 60MB.";
+		lang = true;
+	}
+}
+
+function downDemAlllSchleife() {
+	if (downloading == true) {
+		if (b <= 7) {
+			b++;
+		} else {
+			count = 0;
+			a++;
+			b = 1;
+		}
+		if (birdNameChange.length >= a) {
+			// if array index exists "c" var use
+
+			c = a - 1;
+			console.log(count);
+			if (birdNameChange[c].birdImgPath != 00 && count == 0) {
+				console.log("major" + a + "minor" + b);
+				console.log("hier bild1");
+				downloadInit();
+				progressInfoBar();
+				count++;
+			} else if (birdNameChange[c].birdImgPath1 != 00 && count == 1) {
+				console.log("hier bild2");
+				downloadInit();
+				progressInfoBar();
+				count++;
+			} else if (birdNameChange[c].birdImgPath2 != 00 && count == 2) {
+				console.log("hier bild3");
+				downloadInit();
+				progressInfoBar();
+				count++;
+			} else if (birdNameChange[c].birdImgPath3 != 00 && count == 3) {
+				console.log("hier bild4");
+				downloadInit();
+				progressInfoBar();
+				count++;
+			} else if (birdNameChange[c].birdImgPath4 != 00 && count == 4) {
+				console.log("hier bild5");
+				downloadInit();
+				progressInfoBar();
+				count++;
+			} else if (birdNameChange[c].birdImgPath5 != 00 && count == 5) {
+				console.log("hier bild6");
+				downloadInit();
+				progressInfoBar();
+				count++;
+			} else if (birdNameChange[c].birdImgPath6 != 00 && count == 6) {
+				console.log("hier bild7");
+				downloadInit();
+				progressInfoBar();
+				count++;
+			} else if (birdNameChange[c].birdImgPath7 != 00 && count == 7) {
+				console.log("hier bild6");
+				downloadInit();
+				progressInfoBar();
+				count++;
+			} else {
+				progressInfoBar();
+				downDemAlllSchleife();
+			}
+		} else {
+			if (somethingWentWrong == true) {
+				alert("DOWNLOAD IS FINISHED BUT SOME IMAGES ARE MISSING, IN THE ABOUT MENU YOU CAN TRY IT AGAIN");
+				$("#downWrapID").hide();
+			} else {
+				alert("ALL IMAGES ARE DOWNLOADED");
+				$("#downWrapID").hide();
+				allDonwloaded = "downloaded";
+				localStorage.setItem("isAllDownloaded", "downloaded");
+			}
+		}
+	}
+}
+
+function progressInfoBar() {
+	if (a == progress) {
+		progressInfo += 98.5 / birdNameChange.length;
+		progress++;
+		$("#progress-bar").css("width", progressInfo + "%");
+		var integerNumber = parseInt(progressInfo);
+		$("#progressTEXT").html(integerNumber + "%");
+		console.log(progress + "progress");
+	}
+}
+
+function downloadInit() {
+
+	if (a <= 216) {
+		if (b == 1) {
+			birdDownloadPath = "http://xuanthuy.philipp-m.de/birds/images/birds/" + a + ".jpg";
+			console.log(birdDownloadPath + " download next bird progress");
+			fileNameDirectory = cordova.file.dataDirectory + a + ".jpg";
+		} else {
+			birdDownloadPath = "http://xuanthuy.philipp-m.de/birds/images/birds/" + a + "_" + b + ".jpg";
+			fileNameDirectory = cordova.file.dataDirectory + a + "_" + b + ".jpg";
+		}
+		window.resolveLocalFileSystemURL(fileNameDirectory, downDemAlllSchleife, downloadTheNext);
+	}
+}
+
+var anotherTry = 0;
+function downloadTheNext() {
+	var fileTransfer = new FileTransfer();
+	var trustHosts = true;
+	var ecodeURL = encodeURI(birdDownloadPath);
+	fileTransfer.download(ecodeURL, fileNameDirectory, function(entry) {
+		console.log(fileNameDirectory);
+		downDemAlllSchleife();
+	}, function(error) {
+		console.log("download error source " + error.source);
+		console.log("download error target " + error.target);
+		console.log("upload error code" + error.code);
+		if (anotherTry == 0) {
+			anotherTry++;
+			downloadTheNext();
+		} else {
+			//alert("check your Internet connection! and push ok!");
+			if (anotherTry <= 1) {
+				anotherTry++;
+				downloadTheNext();
+			} else {
+				downDemAlllSchleife();
+				anotherTry = 0;
+				somethingWentWrong = true;
+			}
+		}
+
+	}, false, {
+		headers : {
+			"Authorization" : "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+		}
+	});
+}
